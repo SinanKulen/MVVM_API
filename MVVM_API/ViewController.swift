4d0c5314249ac8deb1cfdcfad9ad8e8b49b17e3b
Sinan Kulen <sinan.kulen@gmail.com>
2021-11-01 15:27:07 +0300

    Initial Commit

---
+++ MVVM_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_API
+//
+//  Created by Sinan Kulen on 1.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
